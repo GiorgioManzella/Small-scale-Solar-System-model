1887785c920e1caac81c74dab50b6207c6ad1ced
giorgio <giorgiolink94@hotmail.it>
2022-04-24 13:22:01 +0100

    Update

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ camera.position.setZ(30);
 
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
 const material = new THREE.MeshStandardMaterial({
-  color: 0xff6347,
+  color: 0x020204,
 });
 
 // define the object and his components*************************
@@ -63,7 +63,7 @@ const lightHelper = new THREE.PointLightHelper(firstLight);
 scene.add(lightHelper);
 
 const gridHelper = new THREE.GridHelper(200, 50);
-scene.add(gridHelper);
+//scene.add(gridHelper);
 
 // set up orbit control after import
 
@@ -90,6 +90,42 @@ Array(600).fill().forEach(addStars);
 const spaceTexture = new THREE.TextureLoader().load("space.jpg");
 scene.background = spaceTexture;
 
+// my image creation
+
+const myTexture = new THREE.TextureLoader().load("./me.jpg");
+
+const me = new THREE.Mesh(
+  new THREE.BoxGeometry(3, 3, 3),
+  new THREE.MeshBasicMaterial({ map: myTexture })
+);
+scene.add(me);
+
+// earth geometry
+const earthTexture = new THREE.TextureLoader().load("earth.jpg");
+const earth = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({ map: earthTexture })
+);
+scene.add(earth);
+earth.position.z = 30;
+earth.position.setX(-10);
+
+// defining the camera movement
+
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  earth.rotation.x + -0.05;
+  earth.rotation.y + -0.075;
+  earth.position.z + -0.05;
+
+  me.rotation.x + -0.01;
+  me.rotation.z + -0.01;
+
+  camera.position.z = t * -0.2;
+  camera.position.x = t * -0.0002;
+  camera.position.y = t * -0.002;
+}
+document.body.Onscroll = moveCamera;
 // create a "infinite-game-loop" to render continuosly
 
 function animate() {
@@ -99,6 +135,8 @@ function animate() {
   torus.rotation.y += 0.001;
   torus.rotation.z += 0.001;
 
+  me.rotation.y += -0.001;
+
   controls.update();
 
   renderer.render(scene, camera);
@@ -107,3 +145,4 @@ function animate() {
 //call the function
 
 animate();
+moveCamera();
